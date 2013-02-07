152c5f1bd506859771a31e42b20b8a59ef6ff389
Robert Heine <rob@zcore.org>
2013-02-07 14:36:33 +0100

    Fixed indents

--- src/main/java/org/zcore/maven/MergeMojo.java
+++ src/main/java/org/zcore/maven/MergeMojo.java
@@ -52,49 +52,49 @@ public class MergeMojo extends AbstractMojo {
      */
     protected OutputStream initOutput(final File file)
         throws MojoExecutionException {
-    // stream to return
-    final OutputStream stream;
-    // plenty of things can go wrong...
-    try {
-        // directory?
-        if (file.isDirectory()) {
-        throw new MojoExecutionException("File "
-            + file.getAbsolutePath() + " is directory!");
-        }
-        // already exists && can't remove it?
-        if (file.exists() && !file.delete()) {
-        throw new MojoExecutionException("Could not remove file: "
-            + file.getAbsolutePath());
-        }
-        // get directory above file file
-        final File fileDirectory = file.getParentFile();
-        // does not exist && create it?
-        if (!fileDirectory.exists() && !fileDirectory.mkdirs()) {
-        throw new MojoExecutionException(
-            "Could not create directory: "
+        // stream to return
+        final OutputStream stream;
+        // plenty of things can go wrong...
+        try {
+            // directory?
+            if (file.isDirectory()) {
+            throw new MojoExecutionException("File "
+                + file.getAbsolutePath() + " is directory!");
+            }
+            // already exists && can't remove it?
+            if (file.exists() && !file.delete()) {
+            throw new MojoExecutionException("Could not remove file: "
+                + file.getAbsolutePath());
+            }
+            // get directory above file file
+            final File fileDirectory = file.getParentFile();
+            // does not exist && create it?
+            if (!fileDirectory.exists() && !fileDirectory.mkdirs()) {
+            throw new MojoExecutionException(
+                "Could not create directory: "
+                    + fileDirectory.getAbsolutePath());
+            }
+            // moar wtf: parent directory is no directory?
+            if (!fileDirectory.isDirectory()) {
+            throw new MojoExecutionException("Not a directory: "
                 + fileDirectory.getAbsolutePath());
+            }
+            // file file is for any reason not creatable?
+            if (!file.createNewFile()) {
+            throw new MojoExecutionException("Could not create file: "
+                + file.getAbsolutePath());
+            }
+            // finally create some file
+            stream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            throw new MojoExecutionException("Could not find file: "
+                + file.getAbsolutePath(), e);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Could not write to file: "
+                + file.getAbsolutePath(), e);
         }
-        // moar wtf: parent directory is no directory?
-        if (!fileDirectory.isDirectory()) {
-        throw new MojoExecutionException("Not a directory: "
-            + fileDirectory.getAbsolutePath());
-        }
-        // file file is for any reason not creatable?
-        if (!file.createNewFile()) {
-        throw new MojoExecutionException("Could not create file: "
-            + file.getAbsolutePath());
-        }
-        // finally create some file
-        stream = new FileOutputStream(file);
-    } catch (FileNotFoundException e) {
-        throw new MojoExecutionException("Could not find file: "
-            + file.getAbsolutePath(), e);
-    } catch (IOException e) {
-        throw new MojoExecutionException("Could not write to file: "
-            + file.getAbsolutePath(), e);
-    }
-    // return
-    return stream;
+        // return
+        return stream;
     }
 
     /**
@@ -105,25 +105,25 @@ public class MergeMojo extends AbstractMojo {
      */
     protected InputStream initInput(final File file)
         throws MojoExecutionException {
-    InputStream stream = null;
-    try {
-        if (file.isDirectory()) {
-        throw new MojoExecutionException("File "
-            + file.getAbsolutePath()
-            + " is directory!");
+        InputStream stream = null;
+        try {
+            if (file.isDirectory()) {
+            throw new MojoExecutionException("File "
+                + file.getAbsolutePath()
+                + " is directory!");
+            }
+            if (!file.exists()) {
+            throw new MojoExecutionException("File "
+                + file.getAbsolutePath()
+                + " does not exist!");
+            }
+            stream = new FileInputStream(file);
+            //append to outfile here
+        } catch (FileNotFoundException e) {
+            throw new MojoExecutionException("Could not find file: "
+                + file.getAbsolutePath(), e);
         }
-        if (!file.exists()) {
-        throw new MojoExecutionException("File "
-            + file.getAbsolutePath()
-            + " does not exist!");
-        }
-        stream = new FileInputStream(file);
-        //append to outfile here
-    } catch (FileNotFoundException e) {
-        throw new MojoExecutionException("Could not find file: "
-            + file.getAbsolutePath(), e);
-    }
-    return stream;
+        return stream;
     }
 
     /**
@@ -132,40 +132,40 @@ public class MergeMojo extends AbstractMojo {
      * @throws MojoFailureException
      */
     public void execute() throws MojoExecutionException, MojoFailureException {
-    // iterate through all <merge />
-    for (Merger merger : mergers) {
-        // get target file name...
-        final File target = merger.getTarget();
-        // get list of source files
-        final List<File> sources = Arrays.asList(merger.getSources());
-        // ...and use a stream
-        final OutputStream targetStream = initOutput(target);
-        // iterate source files
-        for (File source : sources) {
-        final InputStream sourceStream = initInput(source);
-        // append
-        appendStream(sourceStream, targetStream);
-        // close source
-        if (null != sourceStream) {
-            try {
-            sourceStream.close();
-            } catch (IOException e) {
-            throw new MojoExecutionException(
-                "Could not close file: "
-                    + source.getAbsolutePath(), e);
+        // iterate through all <merge />
+        for (Merger merger : mergers) {
+            // get target file name...
+            final File target = merger.getTarget();
+            // get list of source files
+            final List<File> sources = Arrays.asList(merger.getSources());
+            // ...and use a stream
+            final OutputStream targetStream = initOutput(target);
+            // iterate source files
+            for (File source : sources) {
+            final InputStream sourceStream = initInput(source);
+            // append
+            appendStream(sourceStream, targetStream);
+            // close source
+            if (null != sourceStream) {
+                try {
+                sourceStream.close();
+                } catch (IOException e) {
+                throw new MojoExecutionException(
+                    "Could not close file: "
+                        + source.getAbsolutePath(), e);
+                }
+            }
+            }
+            // close target
+            if (null != targetStream) {
+                try {
+                    targetStream.close();
+                } catch (IOException e) {
+                    throw new MojoExecutionException("Could not close file: "
+                        + target.getAbsolutePath(), e);
+                }
             }
         }
-        }
-        // close target
-        if (null != targetStream) {
-        try {
-            targetStream.close();
-        } catch (IOException e) {
-            throw new MojoExecutionException("Could not close file: "
-                + target.getAbsolutePath(), e);
-        }
-        }
-    }
     }
 
     /**
