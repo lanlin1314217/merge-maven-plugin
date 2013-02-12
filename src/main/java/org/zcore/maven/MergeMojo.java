fd1f57de00403067da164aec9bbe8b7f89b471c7
Robert Heine <rob@zcore.org>
2013-02-12 13:06:29 +0100

    Updated comment to have a non-deprecated usage example

--- src/main/java/org/zcore/maven/MergeMojo.java
+++ src/main/java/org/zcore/maven/MergeMojo.java
@@ -29,7 +29,7 @@ public class MergeMojo extends AbstractMojo {
      * <pre>
      * &lt;mergers&gt;
      * &nbsp;&nbsp;&lt;merge&gt;
-     * &nbsp;&nbsp;&nbsp;&nbsp;&lt;target&gt;${build.outputDirectory}/target.txt&lt;/target&gt;
+     * &nbsp;&nbsp;&nbsp;&nbsp;&lt;target&gt;${project.build.outputDirectory}/target.txt&lt;/target&gt;
      * &nbsp;&nbsp;&nbsp;&nbsp;&lt;sources&gt;
      * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;source&gt;src/main/config/${property}/application.txt&lt;/source&gt;
      * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;source&gt;src/main/config/extended/application.txt&lt;/source&gt;
